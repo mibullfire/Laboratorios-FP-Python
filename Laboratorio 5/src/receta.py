b001fb0a4386e5a5b051669cf9bbb1bd42a1f1cc
mibullfire <mibullfire@mail.com>
2024-10-21 10:13:11 +0200

    notacion

--- Laboratorio 5/src/receta.py
+++ Laboratorio 5/src/receta.py
@@ -1,54 +1,59 @@
 import csv
 from datetime import *
-from typing import * # Cuando la NamedTuple está definida con mayúsculas, hay que importarla del typing, no de collections 
+from typing import *  # Cuando la NamedTuple está definida con mayúsculas, hay que importarla del typing, no de collections 
 
-# Namedtuples dadas:
+# Definición de NamedTuples:
 Ingrediente = NamedTuple("Ingrediente",
-					     [("nombre",str),
-						  ("cantidad",float),
-						  ("unidad",str)])
-						 
-Receta = NamedTuple("Receta", 
-                    [("denominación", str),
-                     ("tipo", str),
-                     ("dificultad", str),
-                     ("ingredientes", Optional[List[Ingrediente]]),
-                     ("tiempo", int),
-                     ("calorías", int),
-                     ("fecha", date),
-                     ("precio", float)])
+                         [("nombre", str),  # Nombre del ingrediente
+                          ("cantidad", float),  # Cantidad del ingrediente
+                          ("unidad", str)])  # Unidad de medida del ingrediente
 
-def parse_fecha(cadena:str)->date:
+Receta = NamedTuple("Receta",
+                    [("denominación", str),  # Nombre de la receta
+                     ("tipo", str),  # Tipo de receta (ej. postre, plato principal)
+                     ("dificultad", str),  # Dificultad de la receta
+                     ("ingredientes", Optional[List[Ingrediente]]),  # Lista de ingredientes (puede ser None)
+                     ("tiempo", int),  # Tiempo de preparación en minutos
+                     ("calorías", int),  # Calorías de la receta
+                     ("fecha", date),  # Fecha de creación de la receta
+                     ("precio", float)])  # Precio de la receta
+
+# Función para parsear una fecha desde una cadena
+def parse_fecha(cadena: str) -> date:
     return datetime.strptime(cadena, '%d/%m/%Y').date()
 
-def parse_float(cadena:str)->float:
+# Función para parsear un número flotante desde una cadena
+def parse_float(cadena: str) -> float:
     return float(cadena.replace(',', '.'))
 
-def parse_ingredientes(cadena:str)->Optional[List[Ingrediente]]:
+# Función para parsear una lista de ingredientes desde una cadena
+def parse_ingredientes(cadena: str) -> Optional[List[Ingrediente]]:
     if cadena == '':  
-        return None
+        return None  # Si la cadena está vacía, retorna None
     else:
-        ingredientes = cadena.split(',')
+        ingredientes = cadena.split(',')  # Divide la cadena por comas
         res = []
         
         for i in ingredientes:
-            # fresas-5-u
+            # Ejemplo de formato: fresas-5-u
             nombre, cantidad, unidad = i.split('-')
-            res.append(Ingrediente(nombre, float(cantidad), unidad)) 
+            res.append(Ingrediente(nombre, float(cantidad), unidad))  # Crea un objeto Ingrediente y lo añade a la lista
         return res
 
-def lee_recetas(fichero:str)->list[Receta]:
+# Función para leer recetas desde un fichero CSV
+def lee_recetas(fichero: str) -> list[Receta]:
     res = []
-    with open (fichero, encoding = 'utf-8') as f:
-        lector = csv.reader(f, delimiter = ';')
-        next(lector)
+    with open(fichero, encoding='utf-8') as f:
+        lector = csv.reader(f, delimiter=';')
+        next(lector)  # Salta la cabecera del CSV
 
         for denominacion, tipo, dificultad, ingredientes, tiempo, calorias, fecha, precio in lector:
             res.append(Receta(denominacion, tipo, dificultad, parse_ingredientes(ingredientes), int(tiempo), int(calorias), parse_fecha(fecha), parse_float(precio)))
            
     return res
 
-def diferentes_ingredientes(lista:List[Receta], unidad=None)->int:
+# Función para contar el número de ingredientes diferentes en una lista de recetas
+def diferentes_ingredientes(lista: List[Receta], unidad=None) -> int:
     lista_ingredientes = [i.ingredientes for i in lista if i.ingredientes]
     res = set([ii.nombre for i in lista_ingredientes for ii in i if unidad == None or ii.unidad == unidad])
     ''' Prueba:
@@ -70,7 +75,8 @@ def diferentes_ingredientes(lista:List[Receta], unidad=None)->int:
     '''
     return len(res)
 
-def recetas_con_ingredientes(lista:List[Receta], ingredientes:List[str])->Set[tuple]:
+# Función para obtener recetas que contienen ciertos ingredientes
+def recetas_con_ingredientes(lista: List[Receta], ingredientes: List[str]) -> Set[tuple]:
     res = set()
     for i in lista:
         if i.ingredientes:
@@ -79,13 +85,14 @@ def recetas_con_ingredientes(lista:List[Receta], ingredientes:List[str])->Set[tu
                     res.add((i.denominación, i.calorías, i.precio))
     return res
 
-def receta_mas_barata(lista:List[Receta], tipo:List, num:int=None)->list[Receta]:
+# Función para obtener la receta más barata de un cierto tipo
+def receta_mas_barata(lista: List[Receta], tipo: List, num: int = None) -> list[Receta]:
     res = [i for i in lista if i.tipo in tipo]
     if num:
-        res.sort(key = lambda x: x.calorías)
-        res = res[:num]
-        res.sort(key = lambda x: x.precio)
-        return res[0]
+        res.sort(key=lambda x: x.calorías)  # Ordena por calorías
+        res = res[:num]  # Toma las primeras 'num' recetas
+        res.sort(key=lambda x: x.precio)  # Ordena por precio
+        return res[0]  # Retorna la receta más barata
     else:
-        res.sort(key = lambda x: x.precio)
-        return res[0] 
+        res.sort(key=lambda x: x.precio)  # Ordena por precio
+        return res[0]  # Retorna la receta más barata
